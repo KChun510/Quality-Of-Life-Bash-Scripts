abfa6b470222a8dcbf3a4e3c3ed170159f427daa
WestCoastToast <westcoasttoast@westcoaoastsmbp.mynetworksettings.com>
2024-08-24 16:50:30 -0400

    quick change

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for file in *; do
-	if [[ -f "$file" && "$file" == *.sh && "$file" != README.md ]]; then
+	if [[ -f "$file" && "$file" == *.sh && "$file" != README.md && "$file" != init.sh ]]; then
 		echo "chmod +x $file"
 		chmod +x "$file"
 		cp "$file" ~/
