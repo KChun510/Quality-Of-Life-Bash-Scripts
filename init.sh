fb04f22908ec7d05f41667b0338f0eed6d4b44bf
Kchun510 <kylechun1@gmail.com>
2024-12-12 10:58:03 -0800

    Adding linux mint distro commands

--- init.sh
+++ init.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-for file in *; do
+read -p $'What OS are you running?\n1) MacOs\n2) Linux Mint\nEnter your sys #:' sysNumber
+
+case "$sysNumber" in
+	"1")
+		filePath="./macOS_commands/"
+	;;
+	"2")
+		filePath="./linux_commands/"
+	;;
+	*)	
+	echo "Invalid option: $sysNumber"
+	;;
+esac
+
+for file in "$filePath"/*; do
 	if [[ -f "$file" && "$file" == *.sh && "$file" != README.md && "$file" != init.sh ]]; then
 		echo "chmod +x $file"
 		chmod +x "$file"
 		cp "$file" /usr/local/bin/
 	fi
 done
+
+
+
